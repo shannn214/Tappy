15f1d635f7c7c19bfe8c3a886fce90113fb7fb1d
Grace <gracejin214@gmail.com>
2018-05-08 11:45:58 +0800

    Start game page and finish location function

---
+++ Project2/Controller/Game/GameMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameMapViewController.swift
+//  Project2
+//
+//  Created by 尚靖 on 2018/5/8.
+//  Copyright © 2018年 尚靖. All rights reserved.
+//
+
+import Foundation
+
+class GameMapViewController: UIView {
+    
+}
--- Project2/Controller/Game/GameViewController.swift
+++ Project2/Controller/Game/GameViewController.swift
@@ -7,16 +7,66 @@
 //
 
 import Foundation
+import CoreLocation
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: UIView!
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var movingBtn: UIButton!
+    
+    let locationManager = CLLocationManager()
+    var distance = 0.0
+    var locations = [CLLocation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        NotificationCenter.default.post(name: .loginSuccessfull, object: nil)
+
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        progress.progress = 0
+        movingBtn.isHidden = true
     }
     
-    @IBAction func play(_ sender: Any) {
-        LoginManager.shared.playMusic()
+    override func viewWillAppear(_ animated: Bool) {
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else if CLLocationManager.authorizationStatus() == .denied {
+            print("Please enable getting location.")
+        } else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let curLocation: CLLocation = locations[0]
+        print("\(curLocation.coordinate)")
+        
+        for location in locations as [CLLocation] {
+            if location.horizontalAccuracy < 20 {
+                if self.locations.count > 0 {
+                    distance += location.distance(from: self.locations.last!)
+                    let complete = 25.0
+                    print("-----------")
+                    print(distance)
+                    if distance <= complete {
+                        progress.progress = Float(distance) / Float(complete)
+                    } else {
+                        progress.progress = Float(complete)
+                        movingBtn.isHidden = false
+                    }
+                }
+                self.locations.append(location)
+            }
+        }
+    }
+    
+    @IBAction func movingButton(_ sender: Any) {
+        movingBtn.isHidden = true
+        progress.progress = 0
+        distance = 0
+    }
+    
 }
